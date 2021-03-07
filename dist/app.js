42afe3ded00948e918549f3edaed170123f75014
olympiawoj <owojcik7@gmail.com>
2021-03-07 09:58:51 -0500

    Finishing Drag & Drop

--- dist/app.js
+++ dist/app.js
@@ -42,6 +42,16 @@ class ProjectState extends State {
     addProject(title, description, numofPeople) {
         const newProject = new Project(Math.random().toString(), title, description, numofPeople, ProjectStatus.Active);
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+    moveProject(projectId, newStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -151,8 +161,8 @@ class ProjectList extends Component {
     }
     dropHandler(event) {
         event.preventDefault();
-        console.log('is this running????');
-        console.log(event.dataTransfer.getData('text/plain'));
+        const prjId = event.dataTransfer.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
     dragLeaveHandler(_) {
         const listEl = this.element.querySelector('ul');
--- src/app.ts
+++ src/app.ts
@@ -67,11 +67,25 @@ class ProjectState extends State<Project> {
     // };
 
     this.projects.push(newProject);
+    this.updateListeners()
+
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus){
+    const project = this.projects.find(prj => prj.id === projectId)
+    if(project && project.status !== newStatus){
+      project.status = newStatus;
+      this.updateListeners()
+    }
+  }
+
+  private updateListeners(){
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
   }
 }
+
 
 const projectState = ProjectState.getInstance();
 
@@ -236,8 +250,8 @@ class ProjectList
   @autobind
   dropHandler(event: DragEvent) {
     event.preventDefault()
-    console.log('is this running????')
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
   }
 
   @autobind
