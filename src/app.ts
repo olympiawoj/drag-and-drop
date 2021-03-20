78065742da488df18afdccc660671635592085fe
olympiawoj <owojcik7@gmail.com>
2021-03-20 10:48:27 -0400

    Fix Problem with Namespace Imports

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /// <reference path="models/drag-drop.ts"/>
 /// <reference path="models/project.ts"/>
 /// <reference path="state/project-state.ts"/>
-/// <reference path="util/validate.ts"/>
+/// <reference path="util/validation.ts"/>
 /// <reference path="decorators/autobind.ts"/>
 /// <reference path="components/project-input.ts"/>
 /// <reference path="components/project-list.ts"/>
