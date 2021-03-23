e0bd86332262d3ea1292cfab35b9ba2e493921ae
olympiawoj <owojcik7@gmail.com>
2021-03-23 09:11:14 -0400

    Understanding various Import & Export Syntaxes

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,5 +1,5 @@
 // Component Base Class
-export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+export default abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
     hostElement: T;
     element: U;
